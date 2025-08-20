bf9ae378cb009f737413b9e7204bcfd31ed542c1
Harman Nagra <0769783@MY.SCCCD.EDU>
2025-08-20 11:37:32 -0700

    Started implementation: added constructor

--- Pair.cpp
+++ Pair.cpp
@@ -1,4 +1,12 @@
 // Pair.cpp
-// Your names
+// Harman Nagra, Karina Hernandez, Alejandro Yepez
 // Implement member functions here
 
+
+#include <Pair.h>
+
+Template <typename T>
+Pair <T>::Pair(T f, T s) (
+	f = first;
+	s = second;
+)
